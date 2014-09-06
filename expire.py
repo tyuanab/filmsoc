be6ee081426cca24a7e02196a697271e154bd8a4
JeffOwOSun <sunyushi9546@gmail.com>
2014-09-06 22:18:07 +0800

    modify expire script

--- expire.py
+++ expire.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*- 
+# -*- coding: utf-8 -*-
 
 # A little script to check member expire date and flag them
 # The script should be run at the beginning of each semester.
@@ -8,13 +8,13 @@ from datetime import date
 from models import User
 from helpers import update_mailing_list
 
-"""Check each member and flag the member type. Update the mailling 
+"""Check each member and flag the member type. Update the mailling
 list when done
 """
 def main():
     user_sq = User.select().where(
     	User.member_type != 'Expired',
-    	User.expire_at < date.today()
+    	User.expire_at <= date.today()
     )
     for user in user_sq:
         user.member_type = 'Expired'
